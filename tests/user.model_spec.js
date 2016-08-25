4628fd5e72bdb627feba7bf4da109fc21ee0c9de
cdcvidal <celine_vidal@natural-solutions.eu>
2016-08-25 17:09:33 +0200

    Remove user name test

--- tests/user.model_spec.js
+++ tests/user.model_spec.js
@@ -12,12 +12,6 @@ describe('User', function() {
         myUser = new User();
     });
 
-    it('should have an empty string as a default name', function(){
-        expect(myUser.get('username')).to.be.a('string');
-        expect(myUser.get('username')).to.be.eql(''); // Test with ==
-        expect(myUser.get('username')).to.be.equal(''); // Test with ===
-    });
-
     it('should have a settable email', function(){
         myUser.set('email', 'skdjfh@skdjfh.fr');
         expect(myUser.get('email')).to.be.a('string');
